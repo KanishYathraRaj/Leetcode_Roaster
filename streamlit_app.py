b4e543ee4af20a3852e4a294fc44773acbfbf7a9
KanishYathraRaj <Kanish.aims@gmail.com>
2024-09-08 02:40:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ def initialize_firebase():
    
     encoded_service_account_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
 
-    st.success(encoded_service_account_json)
+    st.success(len(encoded_service_account_json))
 
     # Decode the JSON string back to its original format
     service_account_json = base64.b64decode(encoded_service_account_json).decode()
